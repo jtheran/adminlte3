2fa75a4f1873fe63f81548126fb3b538a79c2940
JMTB <jtheran@lsv-tech.com>
2024-04-10 09:13:11 -0500

    fix de model, controller

--- app/Http/Controllers/ParentController.php
+++ app/Http/Controllers/ParentController.php
@@ -1,10 +1,64 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Parents;
 use Illuminate\Http\Request;
 
 class ParentController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Parents $Parents)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Parents $Parents)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Parents $Parents)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Parents $Parents)
+    {
+        //
+    }
 }
