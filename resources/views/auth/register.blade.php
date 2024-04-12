717de0e2343f01ebf3f2cb2555caeb85ce91584b
JMTB <jtheran@lsv-tech.com>
2024-04-12 10:53:59 -0500

    fix: views auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,7 @@
 @extends('adminlte::auth.register')
+
+
+@section('css')
+    <link rel="stylesheet" href="/styles/barcode-create.css">
+    <link rel="icon" href="/logo.jpg" type="image/jpeg">
+@stop
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,1 +1,8 @@
 @extends('adminlte::auth.verify')
+
+
+@section('css')
+    <link rel="stylesheet" href="/styles/barcode-create.css">
+    <link rel="icon" href="/logo.jpg" type="image/jpeg">
+@stop
+
